7077439c52be6ac2facd64bfaaf4da38bc2d1f9d
AhmedShalaby <ahmedmoustfa144@gmail.com>
2024-10-25 02:10:10 +0300

    last4

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use('/api/auth', authRoutes);
 // Use organization routes
 app.use('/api/organizations', organizationRoutes);
 
+// Root route for testing
+app.get('/', (req, res) => {
+    res.send('API is running');
+  });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
